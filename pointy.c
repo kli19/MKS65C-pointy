80d97f7f0638909939336c76fe71d46e18b2d998
Karen Li <kli19@stuy.edu>
2018-09-16 18:03:35 -0400

    initialized unsigned int, pointer, and print out each individual byte of int

--- pointy.c
+++ pointy.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
+  unsigned int num = 5562918573;
+  char* pointer = &num;
+  
+  printf("unsigned int %d in hex is %x\n", num, num);
+
+  for (int i = 0; i < sizeof(num); i++){
+    pointer++;
+    printf("byte %d: %hhx\n", i, *pointer);
+  }
 
   return 0;
 
